756bfd28510dacc6ce4a76a0fc22b5cfa9d84e93
gzowski <flexyjerkov@gmail.com>
2021-01-22 22:59:13 +0000

    Update kernel.sh

--- kernel.sh
+++ kernel.sh
@@ -1,4 +1,17 @@
 #!/usr/bin/env bash
+#For arch users using Polybar,
+#Requirements: FontAwesome 5 Free, Polybar
+#Checks kernel version against latest version available on archlinux.org packages page
+#If outdated, updates icon alongside kernel version on polybar.
+
+#Add to polybar config
+#[module/kernel]
+#type = custom/script
+#exec = "Script location"/kernel.sh
+#interval = 600
+#format = "<label>"
+#label = %output%
+
 url='https://archlinux.org/packages/core/x86_64/linux/'
 html=$( curl -# -L "${url}" 2> '/dev/null' )
 latest=$(
